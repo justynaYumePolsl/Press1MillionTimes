2bdb1c2835be610d0ae670e60fd2ddb0505c4617
justynaYumePolsl <justwal728@student.polsl.pl>
2022-06-20 12:44:44 +0200

    app files

---
+++ Press1MTimes/app/build/generated/source/buildConfig/debug/com/merive/press1mtimes/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.merive.press1mtimes;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.merive.press1mtimes";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 54;
+  public static final String VERSION_NAME = "2.3.1";
+}
---
+++ Press1MTimes/app/src/main/java/com/merive/press1mtimes/utils/SplashTexts.java
@@ -0,0 +1,6 @@
+package com.merive.press1mtimes.utils;
+
+public enum SplashTexts {
+
+    P1MT, MILLION, PRESS, CLICK, HEY, ONE, BUTTON, SCORESHARE, SCORE, SNOW
+}
